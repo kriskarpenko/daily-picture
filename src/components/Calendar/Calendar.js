98043cb19f243ce809aa3f2af812899157407654
Kristina Karpenko <karpenko.krista@gmail.com>
2022-10-13 16:23:13 +0200

    feat: Calendar component

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,16 @@
+import styles from "./Calendar.module.css";
+
+const Calendar = ({ date = "2004-08-20", picture = "/default.png" }) => {
+  return (
+    <div>
+      <img
+        className={styles.picture}
+        src={picture}
+        alt={`Picture for ${date}`}
+      />
+      <h2>{date}</h2>
+    </div>
+  );
+};
+
+export default Calendar;
